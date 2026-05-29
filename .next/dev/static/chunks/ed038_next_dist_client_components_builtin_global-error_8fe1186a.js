f94a39af2a9da0d142c3ede1f1ad42d2816866d2
Soft7Technology <aakanksha@soft7.in>
2026-05-29 15:55:06 +0530

    edit

--- .next/dev/static/chunks/ed038_next_dist_client_components_builtin_global-error_8fe1186a.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/ed038_next_dist_b3b607ee._.js"
-],
-    source: "dynamic"
-});
